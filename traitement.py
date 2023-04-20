3276df30c1301da1f113dcea1a3e7572115a2473
ZC <zinedine.chelgham@etu.univ-cotedazur.fr>
2023-04-20 16:23:38 +0200

    traitement de données en python

---
+++ traitement.py
@@ -0,0 +1,74 @@
+import xml.etree.ElementTree as ET
+
+
+# Charger le fichier XML
+tree = ET.parse('agence_sejour_linguistique_bd.xml')
+root = tree.getroot()
+
+# Parcourir tous les éléments de la base de données
+def display_all(root, bd_origin):
+    print("------------------Affichage de la bd %s (%d offres disponibles)------------------" % (bd_origin, len(root.findall('.//offre'))))
+    for offre in root.findall('.//offre'):
+        type_offre = offre.get('type')
+        langue = offre.find(".//langue").text
+        destination = offre.find(".//ville").text + ', ' + offre.find(".//pays").text
+        activites_sportives = offre.find(".//activites_sportives").text
+        activites_culturelles = offre.find(".//activites_culturelles").text
+
+        date = offre.find(".//dates")
+        date_debut = date.find("debut").text
+        date_fin = date.find("fin").text
+
+        client_type = offre.find(".//client").get('type')
+        nom_client = offre.find(".//client/nom").text
+        prenom_client = offre.find(".//client/prenom").text
+        age_client = offre.find(".//client/age").text
+
+        print("Type d'offre : ", type_offre)
+        print("Langue : ", langue)
+        print("Destination : ", destination)
+        print("Activités sportives : ", activites_sportives)
+        print("Activités culturelles : ", activites_culturelles)
+        print("Date debut : ", date_debut)
+        print("Date fin : ", date_fin)
+        print("Type de client : ", client_type)
+        print("Nom du client : ", nom_client)
+        print("Prénom du client : ", prenom_client)
+        print("Âge du client : ", age_client)
+        print("--------------")
+
+display_all(root, "orignale")
+
+
+# Récupération des offres d'un client donné
+def get_offres_client(nom, prenom):
+    offres = []
+    for offre in root.findall('.//offre'):
+        nom_client = offre.find(".//client/nom").text
+        prenom_client = offre.find(".//client/prenom").text
+        if nom_client.lower() == nom.lower() and prenom_client.lower() == prenom.lower():
+            offres.append(offre)
+    return offres
+
+# Récupération des offres d'un client donné
+print("------------------Affichage des offres d'un client donné (DUPONT Paul)------------------")
+offres = get_offres_client("DUPONT", "Paul")
+for offre in offres:
+    print(ET.tostring(offre, encoding='unicode'))
+
+
+
+#Suppression de toutes les offres d'un client donné et génération d'un nouveau fichier XML
+def delete_offres_client(nom, prenom):
+    offres = get_offres_client(nom, prenom)
+    for offre in offres:
+        root.remove(offre)
+    tree.write('agence_sejour_linguistique_bd_scripted.xml')
+
+delete_offres_client("DUPONT", "Paul")
+# Charger le fichier XML
+tree = ET.parse('agence_sejour_linguistique_bd_scripted.xml')
+root = tree.getroot()
+display_all(root, "modifiée")
+
+
